0f313d754aaeac72dec51597adb518729b3626c6
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-07-13 18:18:10 -0300

    :sparkles: Update: Type - IVerifier

--- the-mask/src/@types/verifier.d.ts
+++ the-mask/src/@types/verifier.d.ts
@@ -1,7 +1,10 @@
 import { IRules } from './rules';
 export interface IVerifier {
     rules: IRules
-    verify: () => [boolean]
-    apply?: () => [boolean]
+    verify: (content: string) => Map<string, boolean>
+    apply?: (content: string) => Map<string, boolean>
 }
 
+export interface IVerifyResult {
+
+}
