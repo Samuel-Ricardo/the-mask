cc57e78f67cbc80984316595355412a02176a17a
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-07-13 00:21:08 -0300

    :sparkles: Create: Type - ICaracterRule

--- the-mask/src/@types/caracter-rules.d.ts
+++ the-mask/src/@types/caracter-rules.d.ts
@@ -5,3 +5,9 @@ export interface ICaracterRules {
     all_upercases?: boolean
     all_lowercase?: boolean
 }
+
+export interface ICaracterRule{
+    HAVE_LETTERS: (a:string, z:string, allowUpercase:boolean) => string,
+    HAVE_NUMBER: (init:string, end:string) => string
+    HAVE_SPECIAL_CARACTERS: (have:boolean, caracters:[string]) => string
+}
