83e78b7002459af19bb842e44d61b37e6f0e46db
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-07-13 00:26:49 -0300

    :sparkles: Create: implementation - caracter_rules (caracter_rules.ts)

--- the-mask/src/impl/caracter_rules.ts
+++ the-mask/src/impl/caracter_rules.ts
@@ -21,3 +21,16 @@ const filterEspecialCaracter = (caracters:[string]) => {
     })
   } 
 
+export const caracter_rules: ICaracterRule = {
+    HAVE_LETTERS: (a, z, allowUpercase) => allowUpercase ? `${a.toLowerCase()}-${z.toLowerCase()}${a.toUpperCase()}-${z.toUpperCase()}` : `${a}-${z}`,
+
+    HAVE_NUMBER: (init:string, end:string) => `${init}-${end}`,
+    
+    HAVE_SPECIAL_CARACTERS: (have, caracters) => {
+      const regex = ``
+      
+      if(have)filterEspecialCaracter(caracters).forEach(caracter => {regex.concat(caracter)});
+      
+      return regex
+    }
+}
