8d785af7637a74675d7f0d9a8c1a9a4900900458
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-07-13 18:29:31 -0300

    :sparkles: Create: Type - ICaracterRulesKeys

---
+++ the-mask/src/@types/keys/caracters.d.ts
@@ -0,0 +1,7 @@
+export interface ICaracterRulesKeys {
+    must_have: (caracter:String) => string,
+    dont_have: (caracter:String) => string,
+    all_upercase: string,
+    all_lowercase: string,
+}
+
