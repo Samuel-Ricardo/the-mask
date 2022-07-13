e6791a42f37063c8e374301ed7f6cf33e6be8b18
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-07-13 18:35:06 -0300

    :sparkles: Update: Type - IDefaultCaracterRulesKeys

--- the-mask/src/@types/keys/caracters.d.ts
+++ the-mask/src/@types/keys/caracters.d.ts
@@ -5,9 +5,9 @@ export interface ICaracterRulesKeys {
     all_lowercase: string,
 }
 
-export interface IDefaultCaracterRulesKeys {
+export interface IDefaultCaracterRulesKeys extends ICaracterRulesKeys {
     must_have: (caracter:String) => `must_have_${caracter}`,
-        dont_have: (caracter:String) => `dont_have_${caracter}`,
-        all_upercase: `all_upercase`,
-        all_lowercase: `all_lowercase`,
+    dont_have: (caracter:String) => `dont_have_${caracter}`,
+    all_upercase: `all_upercase`,
+    all_lowercase: `all_lowercase`,
 }
