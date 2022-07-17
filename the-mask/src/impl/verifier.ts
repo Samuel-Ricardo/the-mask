817dace245deb9c1ffcfaec8a5f4084e24ff975a
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-07-17 00:06:28 -0300

    :recycle: Update: Fun - verify (verifier.ts)

--- the-mask/src/impl/verifier.ts
+++ the-mask/src/impl/verifier.ts
@@ -33,7 +33,16 @@ export const getVerifier = (rules: IRules):IVerifier => {
             if(max_length) results.set(RULE_KEYS.max_length, content.length <= max_length)
             if(min_length) results.set(RULE_KEYS.min_length, content.length >= min_length)
 
-            
+            if(caracters) {
+                if(caracters.all_lowercase) results.set(RULE_KEYS.caracters.all_lowercase, content.toLowerCase() === content)
+                if(caracters.all_upercase) results.set(RULE_KEYS.caracters.all_upercase, content.toUpperCase() === content)
+                
+                if(caracters.dont_have) caracters.dont_have.forEach((caracter) => results.set(RULE_KEYS.caracters.dont_have(caracter), !content.includes(caracter)))
+                if(caracters.must_have) caracters.must_have.forEach((caracter) => results.set(RULE_KEYS.caracters.must_have(caracter), content.includes(caracter)))
+            }
+
+
+
             return results;
         },
         apply(content) {
