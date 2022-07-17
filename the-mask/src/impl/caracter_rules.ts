93e4811ce5e46a4d43746b0c52d306efe4990cf5
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-07-16 23:46:45 -0300

    :pencil2: Update: Enum - caracter_rules

--- the-mask/src/impl/caracter_rules.ts
+++ the-mask/src/impl/caracter_rules.ts
@@ -1,35 +1,14 @@
-import { ICaracterRule } from "app-types";
+import { IFormatedCaracterRule } from "app-types";
 
-const filterEspecialCaracter = (caracters:[string]) => {
-    return caracters.map((caracter) => {
-        if(
-          caracter === "." ||
-          caracter === "+" ||
-          caracter === "*" ||
-          caracter === "?" ||
-          caracter === "^" ||
-          caracter === "$" ||
-          caracter === "(" ||
-          caracter === ")" ||
-          caracter === "[" ||
-          caracter === "]" ||
-          caracter === "{" ||
-          caracter === "}" ||
-          caracter === "|" ||
-          caracter === "\\"
-        ) {return `\\${caracter}`} else {return caracter}
-    })
-  } 
+export const caracter_rules: IFormatedCaracterRule = {
+    HAVE_LETTERS: ({a, z, allowUpercase}) => allowUpercase ? `${a.toLowerCase()}-${z.toLowerCase()}${a.toUpperCase()}-${z.toUpperCase()}` : `${a}-${z}`,
 
-export const caracter_rules: ICaracterRule = {
-    HAVE_LETTERS: (a, z, allowUpercase) => allowUpercase ? `${a.toLowerCase()}-${z.toLowerCase()}${a.toUpperCase()}-${z.toUpperCase()}` : `${a}-${z}`,
-
-    HAVE_NUMBER: (init:string, end:string) => `${init}-${end}`,
+    HAVE_NUMBER: ({init, end}) => `${init}-${end}`,
     
-    HAVE_SPECIAL_CARACTERS: (have, caracters) => {
+    HAVE_SPECIAL_CARACTERS: ({have, caracters}) => {
       const regex = ``
       
-      if(have)filterEspecialCaracter(caracters).forEach(caracter => {regex.concat(caracter)});
+      if(have)filterEspecialCaracters(caracters).forEach(caracter => {regex.concat(caracter)});
       
       return regex
     }
