be0279b7def83bac04e3975cd2d22efee6713281
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-07-16 23:39:35 -0300

    :pencil2: Update: Type - IRuleKeys

--- the-mask/src/@types/keys/rules.d.ts
+++ the-mask/src/@types/keys/rules.d.ts
@@ -1,14 +1,15 @@
+import { ICaracterRulesKeys, IDefaultCaracterRulesKeys } from './caracters';
 export interface IRuleKeys {
-    caracters?: {},
+    caracters?:  ICaracterRulesKeys,
     format?: {},
-    min_length: string,
-    fixed_length: string,
-    max_length: string,
+    min_length?: string,
+    fixed_length?: string,
+    max_length?: string,
 } 
 
 export interface IDefaultRuleKeys extends IRuleKeys{
-    caracters?: {},
-    format?: {},
+    caracters: IDefaultCaracterRulesKeys,
+    format: {},
     min_length: `min_length`,
     fixed_length: `fixed_length`,
     max_length: `max_length`,
