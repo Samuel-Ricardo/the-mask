4ecf40d456172fa980c5069403747f657ed7dd0e
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-07-16 23:26:53 -0300

    Update: Type - IFormatedCaracterRule

--- the-mask/src/@types/caracter-rules.d.ts
+++ the-mask/src/@types/caracter-rules.d.ts
@@ -2,12 +2,14 @@
 export interface ICaracterRules {
     must_have?: [string]
     dont_have?: [string]
-    all_upercases?: boolean
+    all_upercase?: boolean
     all_lowercase?: boolean
 }
 
-export interface ICaracterRule{
-    HAVE_LETTERS: (a:string, z:string, allowUpercase:boolean) => string,
-    HAVE_NUMBER: (init:string, end:string) => string
-    HAVE_SPECIAL_CARACTERS: (have:boolean, caracters:[string]) => string
+export interface IFormatedCaracterRule{
+    PROPS: IApplyFormatedCaracterRuleProps
+    HAVE_LETTERS: (props: IHaveLettersProps) => string
+    HAVE_NUMBER: (props: IHaveNumbersProps) => string
+    HAVE_SPECIAL_CARACTERS: (props: IHaveSpecialCaractersProps) => string
+    APPLY: (props: IApplyFormatedCaracterRuleProps) => string
 }
