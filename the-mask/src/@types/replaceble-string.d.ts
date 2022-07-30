41eacf338a721a636cdd6934181f9a61c8dfc145
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-07-30 17:24:57 -0300

    :bug: fix: type

--- the-mask/src/@types/replaceble-string.d.ts
+++ the-mask/src/@types/replaceble-string.d.ts
@@ -2,5 +2,5 @@ import { IFormatedCaracterRule } from "./caracter-rules";
 
 export interface IReplacebleString{
     key: string;
-    value: [IFormatedCaracterRule]
+    value: IFormatedCaracterRule[]
 }
